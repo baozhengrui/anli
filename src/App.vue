f5b73bb1885c663bee056b82906f540faab4f248
baozhengrui <bzr306357@126.com>
2021-11-15 14:08:55 +0800

    代码

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-container">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Button>Hello Vue3</Button>
+    <div>
+      <SearchInput />
+    </div>
+  </div>
+</template>npm init @vitejs/app my-vue-app -- --template vue
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SearchInput from './components/SearchInput/index.vue'
+
+
+export default defineComponent({
+  components: {
+    SearchInput
+  }
+})
+
+</script>
+
+<style>
+</style>
---
+++ src/components/SearchInput/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <input class="input" v-bind="$attrs" />
+</template>
+
+<style scoped>
+.input{
+    padding: 6px 8px;
+    outline: none;
+    border: 1px solid var(--gray);
+    background-color: var(--gray);
+    border-radius: var(--border-radius);
+}
+</style>
